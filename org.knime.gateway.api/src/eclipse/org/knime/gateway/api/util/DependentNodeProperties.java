8b561f953836228f7a663c1a72cfa854d655f552
Benjamin Moser <benjamin.moser@knime.com>
2022-01-27 15:34:52 +0100

    NXT-847: Fixes for NXT-847 and NXT-641
    - NXT-847: Do not allow step/resume if loop tail is waiting
    - NXT-641: Do not allow reset if loop is paused
    - Adjusted unit tests
    - Minor code cleanup
    
    NXT-847 (While loop is running, \)

--- org.knime.gateway.api/src/eclipse/org/knime/gateway/api/util/DependentNodeProperties.java
+++ org.knime.gateway.api/src/eclipse/org/knime/gateway/api/util/DependentNodeProperties.java
@@ -60,6 +60,7 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import org.knime.core.node.workflow.ConnectionContainer;
+import org.knime.core.node.workflow.LoopEndNode;
 import org.knime.core.node.workflow.NativeNodeContainer;
 import org.knime.core.node.workflow.NodeContainer;
 import org.knime.core.node.workflow.NodeContainerState;
@@ -80,7 +81,7 @@ import org.knime.core.node.workflow.virtual.subnode.VirtualSubNodeOutputNodeMode
  * calculation of those dependent node properties) should be covered by and tightly integrated into the workflow-manager
  * API/framework.
  *
- * Another note: every time the {@link #update()}-method is called the entire workflow (of the associated workflow
+ * Another note: every time the {@link #calc()}-method is called the entire workflow (of the associated workflow
  * manager and partly of the parent workflow manager) graph is traversed. That maybe could be avoided somehow if we knew
  * what parts of the graph have been changed (a changed node state itself is not enough).
  *
@@ -151,8 +152,34 @@ public final class DependentNodeProperties {
         if (!m_props.containsKey(id)) {
             throw new NoSuchElementException("No pre-calculated can-reset predicate found for node id " + id);
         }
-        return m_wfm.canResetNode(id, i -> m_props.get(i).hasExecutingSuccessors());
+        boolean wfmCanReset = m_wfm.canResetNode(id, i -> m_props.get(i).hasExecutingSuccessors());
+        boolean hasPausedSuccessor = m_props.get(id).hasPausedSuccessor();
+        // Applies e.g. to incoming branches of loops or nodes upstream of the loop head.
+        // Will not be true if there is a paused successor in a parent workflow. However, then, the workflow manager
+        //  will not allow to reset the node by means of canResetContainedNodes -> canResetSuccessors,
+        //  which checks for isExecutionInProgress, which also applies to paused tail nodes.
+        return  wfmCanReset && !hasPausedSuccessor;
     }
+
+    /**
+     * Determine whether nodes in the "loop body" are currently executing, based on the current dependent node properties.
+     * Here, the loop body is the set of nodes forward-reachable from the loop head, up to the loop tail.
+     * Consequently, this includes outgoing branches and does not include incoming branches.
+     * @param tail The tail node of the loop, expected to be an instance of {@link LoopEndNode}.
+     * @return A boolean indicating whether nodes in the loop body (as defined above) are currently executing.
+     */
+    public boolean hasExecutingLoopBody(final NativeNodeContainer tail) {
+        if (!tail.isModelCompatibleTo(LoopEndNode.class)) {
+            throw new IllegalArgumentException("Given node must be a loop end node");
+        }
+
+        return CoreUtil.getLoopContext(tail).map(loopContext -> {
+            boolean sHead = m_props.get(loopContext.getHeadNode()).hasExecutingSuccessors();
+            boolean sTail = m_props.get(tail.getID()).hasExecutingSuccessors();
+            return sHead && !sTail;
+        }).orElse(false);
+    }
+
 
     /**
      * Calculates the node dependent properties. The respective workflow need to be locked when the calculation is
@@ -162,12 +189,14 @@ public final class DependentNodeProperties {
         assert m_wfm.isLockedByCurrentThread();
         LinkedList<NodeID> hasExecutingSuccessors = new LinkedList<>();
         LinkedList<NodeID> hasExecutablePredecessors = new LinkedList<>();
-        findAndInitStartNodes(m_wfm, m_props, hasExecutingSuccessors, hasExecutablePredecessors);
+        LinkedList<NodeID> hasPausedSuccessors = new LinkedList<>();
+        findAndInitStartNodes(m_wfm, m_props, hasExecutingSuccessors, hasExecutablePredecessors, hasPausedSuccessors);
         if (m_props.size() > m_wfm.getNodeContainers().size()) {
             removeSurplusNodes(m_wfm, m_props);
         }
         updateHasExecutablePredecessorsProperties(hasExecutablePredecessors);
         updateHasExecutingSuccessorsProperties(hasExecutingSuccessors);
+        updateHasPausedSuccessorsProperties(hasPausedSuccessors);
     }
 
     private void updateHasExecutablePredecessorsProperties(final Queue<NodeID> startNodes) {
@@ -176,6 +205,10 @@ public final class DependentNodeProperties {
 
     private void updateHasExecutingSuccessorsProperties(final Queue<NodeID> startNodes) {
         iterateNodes(startNodes, this::hasExecutingSuccessorVisitor, id -> predecessors(id, m_wfm));
+    }
+
+    private void updateHasPausedSuccessorsProperties(final Queue<NodeID> startNodes) {
+        iterateNodes(startNodes, this::hasPausedSuccessorVisitor, id -> predecessors(id, m_wfm));
     }
 
     private static boolean isComponentWFM(final WorkflowManager wfm) {
@@ -200,8 +233,8 @@ public final class DependentNodeProperties {
 
     private static Set<NodeID> predecessors(final NodeID id, final WorkflowManager wfm) {
         if (wfm.containsNodeContainer(id)) {
-            return wfm.getIncomingConnectionsFor(id).stream().filter(cc -> !cc.getSource().equals(wfm.getID()))
-                .map(ConnectionContainer::getSource).collect(Collectors.toSet());
+            return wfm.getIncomingConnectionsFor(id).stream().map(ConnectionContainer::getSource)
+                .filter(source -> !source.equals(wfm.getID())).collect(Collectors.toSet());
         } else {
             return Collections.emptySet();
         }
@@ -209,22 +242,24 @@ public final class DependentNodeProperties {
 
     private static Set<NodeID> successors(final NodeID id, final WorkflowManager wfm) {
         if (wfm.containsNodeContainer(id)) {
-            return wfm.getOutgoingConnectionsFor(id).stream().filter(cc -> !cc.getDest().equals(wfm.getID()))
-                .map(ConnectionContainer::getDest).collect(Collectors.toSet());
+            return wfm.getOutgoingConnectionsFor(id).stream().map(ConnectionContainer::getDest)
+                .filter(dest -> !dest.equals(wfm.getID())).collect(Collectors.toSet());
         } else {
             return Collections.emptySet();
         }
     }
 
-    private static void findAndInitStartNodes(final WorkflowManager wfm, final Map<NodeID, Properties> propsMap,
-        final Queue<NodeID> hasExecutingSuccessors, final Queue<NodeID> hasExecutablePredecessors) {
+    private static void findAndInitStartNodes(final WorkflowManager wfm, final Map<NodeID, Properties> propsMap, final Queue<NodeID> hasExecutingSuccessors, final Queue<NodeID> hasExecutablePredecessors,
+            final LinkedList<NodeID> hasPausedSuccessors) {
         for (NodeContainer nc : wfm.getNodeContainers()) {
             var nodeId = nc.getID();
             var props = propsMap.computeIfAbsent(nodeId, i -> new Properties());
             var nodeState = nc.getNodeContainerState();
             var added = false;
 
-            props.setHasExecutingSuccessors(isExecutionInProgress(nodeState));
+            // Optimisation: Set the property for any waiting node as well, but add only actually executing nodes as seeds.
+            //  (Per definition, waiting nodes must have an upstream node that is executing).
+            props.setHasExecutingSuccessors(isExecutionInProgress(nc));
             if (isExecuting(nodeState)) {
                 hasExecutingSuccessors.add(nodeId);
                 added = true;
@@ -236,12 +271,16 @@ public final class DependentNodeProperties {
                 added = true;
             }
 
+            props.setHasPausedSuccessor(isPausedLoopEndNode(nc));
+            if (isPausedLoopEndNode(nc)) {
+                hasPausedSuccessors.add(nodeId);
+            }
+
             if (!added) {
                 handleNodesAtComponentAndMetanodeBorders(wfm, hasExecutingSuccessors, hasExecutablePredecessors, nc, nodeId,
                     props);
             }
         }
-
     }
 
     /*
@@ -281,12 +320,37 @@ public final class DependentNodeProperties {
         }
     }
 
-    private static boolean isExecutionInProgress(final NodeContainerState s) {
-        return s.isExecutionInProgress() || s.isExecutingRemotely();
+    private static boolean isExecutionInProgress(final NodeContainer nc) {
+        // Paused loop tails have state CONFIGURED_MARKEDFOREXEC but are not considered as executing for the purpose
+        //  of this method since they could in fact also be paused (this paused state is not explicitly reflected in
+        //  the node state but determined via NativeNodeContainer#getLoopStatus).
+        var s = nc.getNodeContainerState();
+        return !isPausedLoopEndNode(nc) && (s.isExecutionInProgress() || s.isExecutingRemotely());
     }
 
     private static boolean isExecuting(final NodeContainerState s) {
         return (s.isExecutionInProgress() && !s.isWaitingToBeExecuted()) || s.isExecutingRemotely();
+    }
+
+    private static boolean isPausedLoopEndNode(final NodeContainer nc) {
+        if (!(nc instanceof NativeNodeContainer)) {
+            return false;
+        }
+        var nnc = (NativeNodeContainer)nc;
+        return nnc.isModelCompatibleTo(LoopEndNode.class) && nnc.getLoopStatus() == NativeNodeContainer.LoopStatus.PAUSED;
+    }
+
+    /**
+     * Determine whether the given node has a direct predecessor that is currently waiting to be executed. Does not
+     * check predecessors outside the current workflow (e.g. via connections coming into a metanode).
+     * @param node The node to consider the predecessors of.
+     * @return True iff the node has a direct predecessor that is currently waiting to be executed.
+     */
+    public boolean hasWaitingPredecessor(final NativeNodeContainer node) {
+        return predecessors(node.getID(), m_wfm).stream()
+                .map(m_wfm::getNodeContainer)
+                .map(NodeContainer::getNodeContainerState)
+                .anyMatch(NodeContainerState::isWaitingToBeExecuted);
     }
 
     private boolean hasExecutablePredecessorVisitor(final NodeID id) {
@@ -309,6 +373,16 @@ public final class DependentNodeProperties {
         }
     }
 
+    private boolean hasPausedSuccessorVisitor(final NodeID id) {
+        Properties p = m_props.get(id);
+        if (p.hasPausedSuccessor()) {
+            return false;
+        } else {
+            p.setHasPausedSuccessor(true);
+            return true;
+        }
+    }
+
     private static SubNodeContainer getParentComponent(final WorkflowManager wfm) {
         return (SubNodeContainer)wfm.getDirectNCParent();
     }
@@ -319,6 +393,8 @@ public final class DependentNodeProperties {
 
         private boolean m_hasExecutingSuccessors;
 
+        private boolean m_hasPausedSuccessor;
+
         Properties() {
             this(false, false);
         }
@@ -328,12 +404,28 @@ public final class DependentNodeProperties {
             m_hasExecutingSuccessors = hasExecutingSuccessors;
         }
 
+        /**
+         * @return Whether the node has a predecessor that is executable. Also considers predecessors across
+         *  component/metanode borders.
+         */
         boolean hasExecutablePredecessors() {
             return m_hasExecutablePredecessors;
         }
 
+        /**
+         * @return Whether the node has a successor that is currently executing. Also considers successors across
+         *  component/metanode borders.
+         */
         boolean hasExecutingSuccessors() {
             return m_hasExecutingSuccessors;
+        }
+
+        /**
+         * @return Whether the node has a successor that is a loop end node ("tail") in a paused state. Note that any such nodes
+         * outside the current workflow (i.e. in a parent workflow) are not considered, see {@link #canResetNode(NodeID)}}.
+         */
+        boolean hasPausedSuccessor() {
+            return m_hasPausedSuccessor;
         }
 
         void setHasExecutablePredecessors(final boolean b) {
@@ -342,6 +434,10 @@ public final class DependentNodeProperties {
 
         void setHasExecutingSuccessors(final boolean b) {
             m_hasExecutingSuccessors = b;
+        }
+
+        void setHasPausedSuccessor(final boolean b) {
+            m_hasPausedSuccessor = b;
         }
     }
 
