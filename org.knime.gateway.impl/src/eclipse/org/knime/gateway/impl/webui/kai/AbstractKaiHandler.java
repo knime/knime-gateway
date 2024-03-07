d4842e5dfb49f00dd0ba4b971d19bdd7c2f0168e
Adrian Nembach <adrian.nembach@knime.com>
2024-03-07 12:23:12 +0100

    AP-21873: Introduce KaiHandlerFactory extension point

---
+++ org.knime.gateway.impl/src/eclipse/org/knime/gateway/impl/webui/kai/AbstractKaiHandler.java
@@ -0,0 +1,78 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME AG, Zurich, Switzerland
+ *  Website: http://www.knime.org; Email: contact@knime.org
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME AG herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ * History
+ *   Mar 6, 2024 (Adrian Nembach, KNIME GmbH, Konstanz, Germany): created
+ */
+package org.knime.gateway.impl.webui.kai;
+
+import java.util.function.Consumer;
+
+import org.knime.gateway.impl.webui.service.events.EventConsumer;
+
+/**
+ * Abstract implementation of a {@link KaiHandler} that provides event handling.
+ *
+ * @author Adrian Nembach, KNIME GmbH, Konstanz, Germany
+ */
+public abstract class AbstractKaiHandler implements KaiHandler {
+
+    private final EventConsumer m_eventConsumer;
+
+    /**
+     * @param eventConsumer for sending events to the frontend
+     */
+    protected AbstractKaiHandler(final EventConsumer eventConsumer) {
+        m_eventConsumer = eventConsumer;
+    }
+
+    /**
+     * @param projectId the id of the current workflow
+     * @return the returned consumer relays events to the frontend (message must be JSON serializable)
+     */
+    public Consumer<Object> getEventConsumer(final String projectId) {
+        return m -> m_eventConsumer.accept("AiAssistantEvent", m, projectId);
+    }
+}
