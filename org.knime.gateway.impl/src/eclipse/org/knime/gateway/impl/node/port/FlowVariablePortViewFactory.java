6a4bfc3c940fd7dbab07419ce5cd50265339b5e9
Kai Franze <kai.franze@knime.com>
2024-11-06 18:36:26 +0100

    NXT-2899: Make 'FlowVariableViewUtil' do the heavy lifting
    NXT-2899 (Large data in flow variable freezes JS thread)

--- org.knime.gateway.impl/src/eclipse/org/knime/gateway/impl/node/port/FlowVariablePortViewFactory.java
+++ org.knime.gateway.impl/src/eclipse/org/knime/gateway/impl/node/port/FlowVariablePortViewFactory.java
@@ -48,32 +48,24 @@
  */
 package org.knime.gateway.impl.node.port;
 
-import java.util.Collection;
 import java.util.Collections;
+import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.function.BiConsumer;
-import java.util.function.Consumer;
 import java.util.function.Supplier;
 
-import org.knime.core.data.DataTableSpec;
-import org.knime.core.data.DataType;
-import org.knime.core.data.RowKey;
-import org.knime.core.data.def.DefaultRow;
-import org.knime.core.data.def.StringCell;
 import org.knime.core.node.BufferedDataTable;
 import org.knime.core.node.port.flowvariable.FlowVariablePortObject;
+import org.knime.core.node.workflow.FlowObjectStack;
 import org.knime.core.node.workflow.NodeOutPort;
 import org.knime.core.ui.CoreUIPlugin;
-import org.knime.core.webui.data.DataServiceContext;
 import org.knime.core.webui.data.InitialDataService;
 import org.knime.core.webui.data.RpcDataService;
 import org.knime.core.webui.node.port.PortContext;
 import org.knime.core.webui.node.port.PortView;
 import org.knime.core.webui.node.port.PortViewFactory;
-import org.knime.core.webui.node.view.flowvariable.FlowVariable;
+import org.knime.core.webui.node.view.flowvariable.FlowVariableViewUtil;
 import org.knime.core.webui.node.view.table.TableViewUtil;
-import org.knime.core.webui.node.view.table.TableViewViewSettings.AUTO_SIZE_COLUMNS;
+import org.knime.core.webui.node.view.table.TableViewViewSettings;
 import org.knime.core.webui.node.view.table.data.TableViewInitialData;
 import org.knime.core.webui.page.Page;
 
@@ -89,29 +81,23 @@ public final class FlowVariablePortViewFactory implements PortViewFactory<FlowVa
      */
     @Override
     public PortView createPortView(final FlowVariablePortObject portObject) {
-        var port = (NodeOutPort)PortContext.getContext().getNodePort();
-        var tableId = "flowvariableview" + port.getConnectedNodeContainer().getID();
-        var fos = port.getFlowObjectStack();
-        Collection<org.knime.core.node.workflow.FlowVariable> variables;
-        if (fos != null) {
-            variables = fos.getAllAvailableFlowVariables().values();
-        } else {
-            variables = Collections.emptyList();
-        }
-        DataTableSpec tableSpec = new DataTableSpec(new String[]{"Owner ID", "Data Type", "Variable Name", "Value"},
-            new DataType[]{StringCell.TYPE, StringCell.TYPE, StringCell.TYPE, StringCell.TYPE});
-        Supplier<BufferedDataTable> bufferedTableSupplier = getFlowVariablesTableSupplier(variables, tableSpec);
-        var tableSettings = TableSpecViewFactory.getSettingsForDataTable(tableSpec);
-        tableSettings.m_showColumnDataType = false;
-        tableSettings.m_showTableSize = false;
-        tableSettings.m_autoSizeColumnsToContent = AUTO_SIZE_COLUMNS.FIT_CONTENT_AND_HEADER;
+        final var port = (NodeOutPort)PortContext.getContext().getNodePort();
+        final var tableId = "flowvariableview" + port.getConnectedNodeContainer().getID();
+        final var variables = Optional.ofNullable(port.getFlowObjectStack())//
+            .map(FlowObjectStack::getAllAvailableFlowVariables) //
+            .map(Map::values) //
+            .orElse(Collections.emptyList());
+
+        final Supplier<BufferedDataTable> bufferedTableSupplier = //
+            () -> FlowVariableViewUtil.getBufferedTable(variables);
+        final Supplier<TableViewViewSettings> settingsSupplier = FlowVariableViewUtil::getSettings;
 
         return new PortView() {
 
             @Override
             public Optional<InitialDataService<TableViewInitialData>> createInitialDataService() {
-                return Optional.of(TableViewUtil.createInitialDataService(() -> tableSettings, bufferedTableSupplier,
-                    null, tableId));
+                return Optional.of(//
+                    TableViewUtil.createInitialDataService(settingsSupplier, bufferedTableSupplier, null, tableId));
             }
 
             @Override
@@ -128,25 +114,6 @@ public final class FlowVariablePortViewFactory implements PortViewFactory<FlowVa
             }
 
         };
-    }
-
-    private static Supplier<BufferedDataTable> getFlowVariablesTableSupplier(
-        final Collection<org.knime.core.node.workflow.FlowVariable> variables, final DataTableSpec tableSpec) {
-        return () -> {
-            var container = DataServiceContext.get().getExecutionContext().createDataContainer(tableSpec);
-            variables.forEach(withCounter((counter, variable) -> {
-                var flowVariable = FlowVariable.create(variable);
-                container.addRowToTable(new DefaultRow(RowKey.createRowKey(counter), flowVariable.getOwnerNodeId(),
-                    flowVariable.getType(), flowVariable.getName(), flowVariable.getValue()));
-            }));
-            container.close();
-            return container.getTable();
-        };
-    }
-
-    private static <T> Consumer<T> withCounter(final BiConsumer<Long, T> consumer) {
-        final var counter = new AtomicLong(0);
-        return item -> consumer.accept(counter.getAndIncrement(), item);
     }
 
 }
