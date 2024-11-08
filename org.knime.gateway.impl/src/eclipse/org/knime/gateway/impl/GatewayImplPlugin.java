fc552e53cee47e7b8622d56096170e06ca5d2e48
Robin Gerling <robin.gerling@knime.com>
2024-11-08 14:54:21 +0100

    UIEXT-2278: Register the XMLValueView via an extension point
    UIEXT-2278 (Extension point for data value views)

--- org.knime.gateway.impl/src/eclipse/org/knime/gateway/impl/GatewayImplPlugin.java
+++ org.knime.gateway.impl/src/eclipse/org/knime/gateway/impl/GatewayImplPlugin.java
@@ -55,11 +55,9 @@ import org.knime.base.data.xml.SvgValue;
 import org.knime.core.customization.APCustomization;
 import org.knime.core.customization.APCustomizationProviderService;
 import org.knime.core.data.image.png.PNGImageValue;
-import org.knime.core.data.xml.XMLValue;
 import org.knime.core.webui.node.port.PortViewManager;
 import org.knime.core.webui.node.port.PortViewManager.PortViewDescriptor;
 import org.knime.core.webui.node.view.table.datavalue.DataValueViewManager;
-import org.knime.gateway.impl.node.datavalueview.code.XMLCodeValueView;
 import org.knime.gateway.impl.node.datavalueview.image.PNGImageValueView;
 import org.knime.gateway.impl.node.datavalueview.image.SvgValueView;
 import org.knime.gateway.impl.node.port.DirectAccessTablePortViewFactory;
@@ -121,8 +119,6 @@ public class GatewayImplPlugin implements BundleActivator {
         DataValueViewManager.registerDataValueViewFactory(PNGImageValue.class, PNGImageValueView::new);
 
         DataValueViewManager.registerDataValueViewFactory(SvgValue.class, SvgValueView::new);
-
-        DataValueViewManager.registerDataValueViewFactory(XMLValue.class, XMLCodeValueView::new);
 
         m_customizationServiceTracker = new ServiceTracker<>(context, APCustomizationProviderService.class, null);
         m_customizationServiceTracker.open();
