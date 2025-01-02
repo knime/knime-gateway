01a0a60972f7e9073e76281227b60f3d6faacda3
Robin Gerling <robin.gerling@knime.com>
2025-01-02 12:06:12 +0100

    UIEXT-2283: Move registration of image data value views to corresponding repositories
    UIEXT-2283 (Refactor existing data value views to be registered via extension point)

--- org.knime.gateway.impl/src/eclipse/org/knime/gateway/impl/GatewayImplPlugin.java
+++ org.knime.gateway.impl/src/eclipse/org/knime/gateway/impl/GatewayImplPlugin.java
@@ -51,15 +51,10 @@ package org.knime.gateway.impl;
 import java.util.List;
 import java.util.Optional;
 
-import org.knime.base.data.xml.SvgValue;
 import org.knime.core.customization.APCustomization;
 import org.knime.core.customization.APCustomizationProviderService;
-import org.knime.core.data.image.png.PNGImageValue;
 import org.knime.core.webui.node.port.PortViewManager;
 import org.knime.core.webui.node.port.PortViewManager.PortViewDescriptor;
-import org.knime.core.webui.node.view.table.datavalue.DataValueViewManager;
-import org.knime.gateway.impl.node.datavalueview.image.PNGImageValueView;
-import org.knime.gateway.impl.node.datavalueview.image.SvgValueView;
 import org.knime.gateway.impl.node.port.DirectAccessTablePortViewFactory;
 import org.knime.gateway.impl.node.port.FlowVariablePortViewFactory;
 import org.knime.gateway.impl.node.port.FlowVariableSpecViewFactory;
@@ -115,10 +110,6 @@ public class GatewayImplPlugin implements BundleActivator {
 
         PortViewManager.registerPortViews("org.knime.core.data.DirectAccessTable", //
             List.of(new PortViewDescriptor("Table", new DirectAccessTablePortViewFactory())), List.of(), List.of(0));
-
-        DataValueViewManager.registerDataValueViewFactory(PNGImageValue.class, PNGImageValueView::new);
-
-        DataValueViewManager.registerDataValueViewFactory(SvgValue.class, SvgValueView::new);
 
         m_customizationServiceTracker = new ServiceTracker<>(context, APCustomizationProviderService.class, null);
         m_customizationServiceTracker.open();
