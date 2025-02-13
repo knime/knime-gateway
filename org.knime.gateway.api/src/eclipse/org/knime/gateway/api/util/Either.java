21c6300cc358b65106a7db4959c35a25d8dab9e4
Benjamin Moser <benjamin.moser@knime.com>
2025-02-13 14:04:36 +0100

    NXT-3176: Provide parent project/component metadata/description in metanode workflows
    (NXT-3176 "Info" panel for metanodes is empty)

---
+++ org.knime.gateway.api/src/eclipse/org/knime/gateway/api/util/Either.java
@@ -0,0 +1,108 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME AG, Zurich, Switzerland
+ *  Website: http://www.knime.org; Email: contact@knime.org
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME AG herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ */
+
+package org.knime.gateway.api.util;
+
+import org.knime.core.node.util.ClassUtils;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+/**
+ * Value type that can hold either of two type instances.
+ */
+public class Either<L, R> {
+
+    L m_left;
+
+    R m_right;
+
+    private Either(final L left, final R right) {
+        m_left = left;
+        m_right = right;
+    }
+
+    /**
+     * @return An instance containing value of type {@link L}
+     */
+    public static <L, R> Either<L, R> left(final L left) {
+        return new Either<>(left, null);
+    }
+
+    /**
+     * @return An instance containing value of type {@link R}
+     */
+    public static <L, R> Either<L, R> right(final R right) {
+        return new Either<>(null, right);
+    }
+
+    /**
+     * @return an instance containing no value
+     */
+    public static <L, R> Either<L, R> empty() {
+        return new Either<>(null, null);
+    }
+
+    /**
+     * Return the value of type {@code Z} if it is set, otherwise an empty optional.
+     */
+    public <Z> Optional<Z> get(final Class<Z> query) {
+        return Stream.of(m_left, m_right) //
+            .filter(query::isInstance) //
+            .map(e -> ClassUtils.castOptional(query, e)) //
+            .flatMap(Optional::stream) //
+            .findFirst();
+    }
+
+    @Override
+    public String toString() {
+        var value = m_left != null ? m_left : m_right;
+        if (value == null) {
+            return "Either[empty]";
+        }
+        return "Either[%s]".formatted(value);
+    }
+}
