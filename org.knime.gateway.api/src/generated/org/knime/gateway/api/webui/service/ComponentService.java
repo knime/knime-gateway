2e687de200d89f8a0295784f62de46dd861fd1c2
Tobias Kampmann <tobias.kampmann@tngtech.com>
2025-02-13 16:57:58 +0100

    NXT-3307: Add reexecution endpoint
    NXT-3307 (ComponentView: new endpoint for reexecution interaction)

--- org.knime.gateway.api/src/generated/org/knime/gateway/api/webui/service/ComponentService.java
+++ org.knime.gateway.api/src/generated/org/knime/gateway/api/webui/service/ComponentService.java
@@ -68,4 +68,18 @@ public interface ComponentService extends GatewayService {
      */
     Object getCompositeViewPage(String projectId, org.knime.gateway.api.entity.NodeIDEnt workflowId, org.knime.gateway.api.entity.NodeIDEnt nodeId)  throws ServiceExceptions.ServiceCallException;
         
+    /**
+     * Update the state a component and reexecute it.
+     *
+     * @param projectId ID of the workflow-project.
+     * @param workflowId The ID of a workflow which has the same format as a node-id.
+     * @param nodeId The ID of a node. The node-id format: Node IDs always start with &#39;root&#39; and optionally followed by numbers separated by &#39;:&#39; referring to nested nodes/subworkflows,e.g. root:3:6:4. Nodes within components require an additional trailing &#39;0&#39;, e.g. &#39;root:3:6:0:4&#39; (if &#39;root:3:6&#39; is a component).
+     * @param nodeIdThatTriggered The ID of the node that triggered the reexecution from within a component, i.e., there is no leading root.
+     * @param stateUpdates 
+     *
+     * 
+     * @throws ServiceExceptions.ServiceCallException If a Gateway service call failed for some reason.
+     */
+    void reexecuteComponentNode(String projectId, org.knime.gateway.api.entity.NodeIDEnt workflowId, org.knime.gateway.api.entity.NodeIDEnt nodeId, String nodeIdThatTriggered, java.util.Map<String, String> stateUpdates)  throws ServiceExceptions.ServiceCallException;
+        
 }
