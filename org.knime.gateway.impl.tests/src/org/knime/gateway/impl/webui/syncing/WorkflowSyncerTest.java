8f2d77d2e8e2d8497eee51835968082ee274ab55
Benjamin Moser <benjamin.moser@knime.com>
2026-01-04 17:14:12 +0100

    adjust javadoc

--- org.knime.gateway.impl.tests/src/org/knime/gateway/impl/webui/syncing/WorkflowSyncerTest.java
+++ org.knime.gateway.impl.tests/src/org/knime/gateway/impl/webui/syncing/WorkflowSyncerTest.java
@@ -42,9 +42,6 @@
  *  may freely choose the license terms applicable to such Node, including
  *  when such Node is propagated with or for interoperation with KNIME.
  * ---------------------------------------------------------------------
- *
- * History
- *   Dec 23, 2025 (assistant): add unit tests
  */
 package org.knime.gateway.impl.webui.syncing;
 
